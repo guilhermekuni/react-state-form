a4a7955e7830bb90dbcc70b3f375fb7427c6c8d3
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-06-14 16:16:50 -0300

    implements input validations

--- src/pages/ClientForm/index.js
+++ src/pages/ClientForm/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { REGEX_EMAIL } from '../../utils/regularExpresions';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
@@ -12,6 +14,10 @@ const ClientForm = () => {
   const [confirmationPassword, setConfirmationPassword] = useState('');
 
   const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handlePasswordErrorValidation = () => password !== confirmationPassword;
+
+  const handleEmailErrorValidation = () => !email || !REGEX_EMAIL.test(email);
 
   const handleSubmit = () => {
     const request = {
@@ -43,21 +49,24 @@ const ClientForm = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             type="email"
-            hasError={formSubmitted && !email}
+            hasError={formSubmitted && handleEmailErrorValidation()}
+            success={formSubmitted && !handleEmailErrorValidation()}
           />
           <Input
             label="Password *"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             type="password"
-            hasError={formSubmitted && !password}
+            hasError={formSubmitted && (!password || handlePasswordErrorValidation())}
+            success={formSubmitted && (password && !handlePasswordErrorValidation())}
           />
           <Input
             label="Confirm your password *"
             value={confirmationPassword}
             onChange={(e) => setConfirmationPassword(e.target.value)}
             type="password"
-            hasError={formSubmitted && !confirmationPassword}
+            hasError={formSubmitted && (!confirmationPassword || handlePasswordErrorValidation())}
+            success={formSubmitted && (confirmationPassword && !handlePasswordErrorValidation())}
           />
           <Button onClick={handleSubmit}>SUBMIT</Button>
         </S.Form>
